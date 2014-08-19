1e5c58b707515c069b9d463a42cd1eeb61cbe1a9
cdeeter <celiadeeter@celiadesmacbook.attlocal.net>
2014-08-18 20:14:46 -0700

    Made app run correctly

--- js/app.js
+++ js/app.js
@@ -1,19 +1,18 @@
 $(document).ready(function() {
-	$('button').click(function() {
-		var fizzBuzz = function (number) {
-		$('.results').empty();
+	$('#button').click(function() {
+		var number = $("#num").val();
 		for (var i = 1; i <= number; i++) {
 			if ( i % 15 === 0) {
-				$('.results').append('FizzBuzz' + '<br/>');
+				$('.results').append('Fizz Buzz' + '<br/>');
 			} else if (i % 5 === 0) {
 				$('.results').append('Buzz' + '<br/>');
 			} else if (i % 3 === 0) {
 				$('.results').append('Fizz' + '<br/>');
 			} else {
-				$('.results').append(i + '<br>');
+				$('.results').append(i + '<br/>');
 			}
 		};
-	};
+	});
 
 
 });
